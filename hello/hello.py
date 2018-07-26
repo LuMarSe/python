742ef6a1291fa8074612e2c81b486782e169aba6
luis <luis.martin@inspiringbenefits.com>
2018-07-26 13:08:59 +0200

    Initial commit

---
+++ hello/hello.py
@@ -0,0 +1,23 @@
+num1 = int(input('Please enter an integer value: '))
+num2 = int(input('Please enter another integer value: '))
+print('Please enter another integer value: ')
+num3 = int(input())
+print(num1, '+', num2, '+',num3, '=', num1 + num2 +  num3)
+
+'''
+x1 = eval(input('Entry x1? '))
+print('x1 =', x1, ' type:', type(x1))
+
+x2 = eval(input('Entry x2? '))
+print('x2 =', x2, ' type:', type(x2))
+
+x3 = eval(input('Entry x3? '))
+print('x3 =', x3, ' type:', type(x3))
+
+x4 = eval(input('Entry x4? '))
+print('x4 =', x4, ' type:', type(x4))
+
+x5 = eval(input('Entry x5? '))
+print('x5 =', x5, ' type:', type(x5))
+
+'''
